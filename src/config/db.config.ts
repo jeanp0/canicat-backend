12a7c1e727414d5d6d9d13595d97a1be813df7cc
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2021-09-01 17:30:01 -0500

    new database config added for deploy

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from 'sequelize';
 
-const host = 'localhost';
-const user = 'root';
-const password = '#MySQL8:Password*';
-const dbName = 'canicatdb';
+const host = 'remotemysql.com';
+const user = 'tXagkjtUSi';
+const password = 'xVwfRhKpgA';
+const dbName = 'tXagkjtUSi';
 
 const db = new Sequelize(
   dbName,
