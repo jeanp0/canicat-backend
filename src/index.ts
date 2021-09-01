9cb7a12a177b718f8c1d49668a1d64c8de04993c
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2021-09-01 18:17:21 -0500

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as expressWinston from 'express-winston';
 import * as http from 'http';
 import * as winston from 'winston';
 import db from './config/db.config';
-import { PORT, STATIC_FILES_DIRECTORY } from './config/routes.config';
+import { PORT } from './config/routes.config';
 import { AuthRoutes } from './routes/auth.routes.config';
 import { CommonRoutesConfig } from './routes/common.routes.config';
 import { PetRoutes } from './routes/pet.routes.config';
@@ -67,7 +67,8 @@ routes.push(new AuthRoutes(app));
 
 // serve static files from "public" directory
 // example: http://localhost:3000/pet_pictures/dogo.jpg
-app.use(express.static(STATIC_FILES_DIRECTORY));
+// app.use(express.static(STATIC_FILES_DIRECTORY));
+app.use(express.static(__dirname + 'public'));
 
 // this is a simple route to make sure everything is working properly
 const runningMessage = `Server running at http://localhost:${process.env.PORT || PORT}`;
