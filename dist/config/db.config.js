a8da4accee808fd3ebc056bcba80d0f97510dcb6
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2021-09-01 23:12:47 -0500

    Heroku

--- dist/config/db.config.js
+++ dist/config/db.config.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const host = 'remotemysql.com';
-const user = 'tXagkjtUSi';
-const password = 'xVwfRhKpgA';
-const dbName = 'tXagkjtUSi';
+const host = 'localhost';
+const user = 'root';
+const password = '#MySQL8:Password*';
+const dbName = 'canicatdb';
 const db = new sequelize_1.Sequelize(dbName, user, password, { host, dialect: 'mysql' });
 exports.default = db;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGIuY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2NvbmZpZy9kYi5jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSx5Q0FBc0M7QUFFdEMsTUFBTSxJQUFJLEdBQUcsaUJBQWlCLENBQUM7QUFDL0IsTUFBTSxJQUFJLEdBQUcsWUFBWSxDQUFDO0FBQzFCLE1BQU0sUUFBUSxHQUFHLFlBQVksQ0FBQztBQUM5QixNQUFNLE1BQU0sR0FBRyxZQUFZLENBQUM7QUFFNUIsTUFBTSxFQUFFLEdBQUcsSUFBSSxxQkFBUyxDQUN0QixNQUFNLEVBQ04sSUFBSSxFQUNKLFFBQVEsRUFDUixFQUFFLElBQUksRUFBRSxPQUFPLEVBQUUsT0FBTyxFQUFFLENBQzNCLENBQUM7QUFFRixrQkFBZSxFQUFFLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGIuY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2NvbmZpZy9kYi5jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSx5Q0FBc0M7QUFFdEMsTUFBTSxJQUFJLEdBQUcsV0FBVyxDQUFDO0FBQ3pCLE1BQU0sSUFBSSxHQUFHLE1BQU0sQ0FBQztBQUNwQixNQUFNLFFBQVEsR0FBRyxtQkFBbUIsQ0FBQztBQUNyQyxNQUFNLE1BQU0sR0FBRyxXQUFXLENBQUM7QUFFM0IsTUFBTSxFQUFFLEdBQUcsSUFBSSxxQkFBUyxDQUN0QixNQUFNLEVBQ04sSUFBSSxFQUNKLFFBQVEsRUFDUixFQUFFLElBQUksRUFBRSxPQUFPLEVBQUUsT0FBTyxFQUFFLENBQzNCLENBQUM7QUFFRixrQkFBZSxFQUFFLENBQUMifQ==
