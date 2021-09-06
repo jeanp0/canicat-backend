d13301c2fd8c6862894cf1cb8327cc5fb0a617ef
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2021-09-06 01:27:21 -0500

    config de database actualizada

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,13 +1,22 @@
 import { Sequelize } from 'sequelize';
 
+// local
 // const host = 'localhost';
 // const user = 'root';
 // const password = '#MySQL8:Password*';
 // const dbName = 'canicatdb';
-const host = 'remotemysql.com';
-const user = 'tXagkjtUSi';
-const password = 'xVwfRhKpgA';
-const dbName = 'tXagkjtUSi';
+
+// remotemysql.com
+// const host = 'remotemysql.com';
+// const user = 'tXagkjtUSi';
+// const password = 'xVwfRhKpgA';
+// const dbName = 'tXagkjtUSi';
+
+// clever-cloud
+const host = 'beq5n68bndidrnhygdx5-mysql.services.clever-cloud.com';
+const user = 'unw8xkglwm0y5tbd';
+const password = 'BTtstddaxxAox20Blaqr';
+const dbName = 'beq5n68bndidrnhygdx5';
 
 const db = new Sequelize(dbName, user, password, { host, dialect: 'mysql' });
 
